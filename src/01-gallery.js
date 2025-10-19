6371ed55ad2eb679b006c61f13850b00b049ac11
Nikichva <niki.22.12.00@gmail.com>
2025-10-20 01:48:53 +0200

    connected simplelightbox and created gallery

--- src/01-gallery.js
+++ src/01-gallery.js
@@ -1,5 +1,8 @@
 'use strict';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const images = [
   {
     preview:
@@ -85,3 +88,8 @@ function createGalleryMarkup(images, gallery) {
 }
 
 createGalleryMarkup(images, gallery);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
